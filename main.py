73fdf207c0977c5374b1e41c19d3cbe3399e519a
turkishASMR <piotrhirkyj@gmail.com>
2024-04-03 21:10:10 +0200

    Minor change

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import PyPDF2
-with open (r'C:\Users\piotr\Desktop\Python Bootcamp\Day 91\sample.pdf', 'rb') as file:
-    reader = PyPDF2.PdfReader(file)
-    page = reader.pages[0]
-    print(page.extract_text(0))
 
 
+#file_path = input("Please provide file path:")
+file_path = 'sample.pdf'
+reader = PyPDF2.PdfReader(file_path)
+no_pages = len(reader.pages)
+# page = reader.pages[0]
+# print(page.extract_text(0))
+
+whole_text = ''
+for i in range(0,no_pages):
+    whole_text += f'Page {i+1}\n'
+    whole_text += f'{reader.pages[i].extract_text(0)}\n'.replace('\n',' ')
+print(whole_text)
